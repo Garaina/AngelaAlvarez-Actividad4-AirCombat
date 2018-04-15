ea10a8e047b6decbb0a80fab624e8c4adc820029
Angela Maria Alvarez <angelaalhe@unisabana.edu.co>
2018-04-14 23:37:24 -0500

    Juego de aviones

---
+++ AngelaAlvarez-AirCombat/Assets/Scripts/Missil.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missil : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// mueve el avion hacia el frente
+		this.transform.Translate(0.0f,0.0f,1.0f);
+
+	}
+	void OnCollisionEnter(Collision collision){
+		//Cuando el misil colisiona con la plataforma la elimina
+		Destroy (collision.collider.gameObject);
+		Destroy (this.gameObject);
+	}
+}
---
+++ AngelaAlvarez-AirCombat/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				this.transform.Translate (0.0f, 0.5f, 0.0f);
+			}
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				this.transform.Translate (0.0f, -0.5f, 0.0f);
+			}
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				this.transform.Translate (0.5f, 0.0f, 0.0f);
+			}
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				this.transform.Translate (-0.5f, 0.0f, 0.0f);
+			}
+
+
+			if (Input.GetKeyDown (KeyCode.Space)) {	
+
+				GameObject missile = GameObject.Instantiate (
+					Resources.Load ("Prefab/Missil")as GameObject);
+				missile.transform.SetParent (this.gameObject.transform);
+				missile.transform.localPosition = new Vector3 (0, -7, 0);
+				missile.transform.SetParent (null);
+
+		}
+	}
+}
